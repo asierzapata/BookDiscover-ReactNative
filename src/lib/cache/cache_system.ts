7381bae0d281d6174f701312e032299c7f4fe4d6
asierzapata <asier.zapata@gmail.com>
2018-07-23 20:34:15 +0200

    Cache wraper

---
+++ src/lib/cache/cache_system.ts
@@ -0,0 +1,38 @@
+
+import { AsyncStorage } from 'react-native'
+
+/* ====================================================== */
+/*                          API                           */
+/* ====================================================== */
+
+export default {
+    getCachedDataByKey,
+    setCachedDataByKey,
+    removeCachedDataByKey,
+    getAllCachedDataKeys,
+    clearAllCachedData
+}
+
+/* ====================================================== */
+/*                    Implementation                      */
+/* ====================================================== */
+
+function getCachedDataByKey(key: string): Promise<string> {
+    return AsyncStorage.getItem(key)
+}
+
+function setCachedDataByKey(key: string, value: string): Promise<void> {
+    return AsyncStorage.setItem(key, value)
+}
+
+function removeCachedDataByKey(key: string): Promise<void>{
+    return AsyncStorage.removeItem(key)
+}
+
+function getAllCachedDataKeys(): Promise<string[]>{
+    return AsyncStorage.getAllKeys()
+}
+
+function clearAllCachedData(): Promise<void> {
+    return AsyncStorage.clear()
+}
